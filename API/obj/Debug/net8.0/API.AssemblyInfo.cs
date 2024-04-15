e494b9acc09e12ef2d6ce96470e426ba45193312
Caroline Madison <carolinemariemadison@gmail.com>
2024-04-14 22:19:01 -0500

    added notes to gitignore

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2597ed32b254fda457caeacae4a99a59c10e4877")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93bf482d8adec49d0bd3bf0861c125143948b838")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
