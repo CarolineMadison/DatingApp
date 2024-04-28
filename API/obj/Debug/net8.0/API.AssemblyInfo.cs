81fd24c3b09718f9e65bf4dfa034fa81560bfc61
Caroline Madison <carolinemariemadison@gmail.com>
2024-04-28 14:16:43 -0500

    home page added

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1a885acfb0ee15e418595b84d66bcf7bc922e97")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+538a4d7045bd45d9e16dd822bde307f9b01a5786")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
 import { FormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { HomeComponent } from './home/home.component';
 
 // This is responsible for loading the angluar application
 //EACH ANGULAR APPLICATION NEEDS AT LEAST ONE MODULE
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
